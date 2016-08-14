6d41e52611d6c3d9e2da4c854d1349f3db101715
Alexander Golovko <alik@csltd.com.ua>
2016-08-14 17:41:19 +0300

    Spring logging using Log4j

--- src/main/java/algol/csltd/com/ua/SpringTest.java
+++ src/main/java/algol/csltd/com/ua/SpringTest.java
@@ -2,6 +2,7 @@ package algol.csltd.com.ua;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 
 public class SpringTest {
 
@@ -9,5 +10,7 @@ public class SpringTest {
 
         Log log = LogFactory.getLog(SpringTest.class);
         log.info("JCL logging...");
+        Logger l = Logger.getLogger(SpringTest.class);
+        l.info("Log4j logging...");
     }
 }
