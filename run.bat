dad581d4acaad243f8c86327e9da5958a5d7e9e6
Alexander Golovko <alik@csltd.com.ua>
2016-08-14 17:09:43 +0300

    Spring Logging

---
+++ run.bat
@@ -0,0 +1,1 @@
+@%M2_HOME%\bin\mvn clean compile exec:java -Dexec.mainClass="algol.csltd.com.ua.SpringTest"
---
+++ src/main/java/algol/csltd/com/ua/SpringTest.java
@@ -0,0 +1,13 @@
+package algol.csltd.com.ua;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class SpringTest {
+
+    public static void main(String[] args) {
+
+        Log log = LogFactory.getLog(SpringTest.class);
+        log.info("JCL logging...");
+    }
+}
