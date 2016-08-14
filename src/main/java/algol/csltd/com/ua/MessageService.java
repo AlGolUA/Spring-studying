4df6aa44f9dbd811c31b509c62e325ee8f940771
Alexander Golovko <alik@csltd.com.ua>
2016-08-14 18:35:56 +0300

    Java based configuration example

---
+++ src/main/java/algol/csltd/com/ua/MessageService.java
@@ -0,0 +1,5 @@
+package algol.csltd.com.ua;
+
+public interface MessageService {
+    String getMessage();
+}
--- src/main/java/algol/csltd/com/ua/SpringTest.java
+++ src/main/java/algol/csltd/com/ua/SpringTest.java
@@ -2,18 +2,25 @@ package algol.csltd.com.ua;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
 
+@Configuration
+@ComponentScan
 public class SpringTest {
 
-    public static void main(String[] args) {
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+                return "Hello World!";
+            }
+        };
+    }
 
-        Log log = LogFactory.getLog(SpringTest.class);
-        log.info("JCL logging...");
-        Logger l = Logger.getLogger(SpringTest.class);
-        l.info("Log4j logging...");
-        org.slf4j.Logger ll = LoggerFactory.getLogger(SpringTest.class);
-        ll.info("SLF4J logging...");
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringTest.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
     }
 }
