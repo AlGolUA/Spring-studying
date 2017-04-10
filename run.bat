eb2e763435271ddcd0b43d5166ddc24af9a778ed
Alexander Golovko <alik@csltd.com.ua>
2017-04-10 11:27:11 +0300

    Demo of several logging frameworks

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-@%M2_HOME%\bin\mvn clean compile exec:java -Dexec.mainClass="algol.csltd.com.ua.SpringTest"
+@chcp 1251 > nul
+call mvn clean compile exec:java -P SLF4J-LOG4J -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
+call mvn clean compile exec:java -P SLF4J-JDK14 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties
+call mvn clean compile exec:java -P SLF4J-Simple -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
+call mvn clean compile exec:java -P SLF4J-LogBack -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
--- src/main/java/algol/csltd/com/ua/SpringTest.java
+++ src/main/java/algol/csltd/com/ua/SpringTest.java
@@ -1,19 +1,29 @@
 package algol.csltd.com.ua;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SpringTest {
 
     public static void main(String[] args) {
+        // Set up JUL config file
+        // System.setProperty("java.util.logging.config.file", "c:\\Users\\admin\\IdeaProjects\\Spring-studying\\src\\main\\resources\\logging.properties");
+        // System property cab be defined also in call of java.exe like java.exe -Djava.util.logging.config.file=logging.properties
+        try {
+            Class<?> aClass = Class.forName("org.slf4j.impl.JDK14LoggerFactory");
+        } catch (ClassNotFoundException e) {
+            // Don't use the SLF4JBridgeHandler for case of JDK14 logging framework to prevent cycling code
+            org.slf4j.bridge.SLF4JBridgeHandler.removeHandlersForRootLogger();
+            org.slf4j.bridge.SLF4JBridgeHandler.install();
+        }
+        // ROOT level of JUL logging should be set to ALL
+        java.util.logging.Logger.getLogger("").setLevel(java.util.logging.Level.ALL);
 
-        Log log = LogFactory.getLog(SpringTest.class);
+        java.util.logging.Logger julLogger = java.util.logging.Logger.getLogger(SpringTest.class.getName());
+        julLogger.info("JUL logging...");
+        julLogger.finest("JUL logging...");
+        org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SpringTest.class);
+        logger.info("SLF4J logging...");
+        logger.trace("SLF4J logging...");
+        org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(SpringTest.class);
         log.info("JCL logging...");
-        Logger l = Logger.getLogger(SpringTest.class);
-        l.info("Log4j logging...");
-        org.slf4j.Logger ll = LoggerFactory.getLogger(SpringTest.class);
-        ll.info("SLF4J logging...");
+        log.trace("JCL logging...");
     }
 }
