d51be44112e45654b5165c4466f830e8209b9875
Alexander Golovko <alik@csltd.com.ua>
2017-04-12 15:33:20 +0300

    расширения для JCL logging

--- run.bat
+++ run.bat
@@ -3,3 +3,4 @@ call mvn clean compile exec:java -P SLF4J-LOG4J -Dexec.mainClass="algol.csltd.co
 call mvn clean compile exec:java -P SLF4J-JDK14 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties
 call mvn clean compile exec:java -P SLF4J-Simple -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
 call mvn clean compile exec:java -P SLF4J-LogBack -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
+call mvn clean compile exec:java -P WO_SLF4J -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties
--- src/main/java/algol/csltd/com/ua/SpringTest.java
+++ src/main/java/algol/csltd/com/ua/SpringTest.java
@@ -1,17 +1,29 @@
 package algol.csltd.com.ua;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ReflectPermission;
+
 public class SpringTest {
 
     public static void main(String[] args) {
-        // Set up JUL config file
-        // System.setProperty("java.util.logging.config.file", "c:\\Users\\admin\\IdeaProjects\\Spring-studying\\src\\main\\resources\\logging.properties");
-        // System property cab be defined also in call of java.exe like java.exe -Djava.util.logging.config.file=logging.properties
+//         Set up JUL config file
+//         System.setProperty("java.util.logging.config.file", "c:\\Users\\admin\\IdeaProjects\\Spring-studying\\src\\main\\resources\\logging.properties");
+//         System property cab be defined also in call of java.exe like java.exe -Djava.util.logging.config.file=logging.properties
         try {
             Class<?> aClass = Class.forName("org.slf4j.impl.JDK14LoggerFactory");
         } catch (ClassNotFoundException e) {
-            // Don't use the SLF4JBridgeHandler for case of JDK14 logging framework to prevent cycling code
-            org.slf4j.bridge.SLF4JBridgeHandler.removeHandlersForRootLogger();
-            org.slf4j.bridge.SLF4JBridgeHandler.install();
+//            Use the SLF4JBridgeHandler only is JDK14 logging framework is not used
+//            org.slf4j.bridge.SLF4JBridgeHandler.removeHandlersForRootLogger();
+//            org.slf4j.bridge.SLF4JBridgeHandler.install();
+            try {
+                Class<?> bridgeHandler = Class.forName("org.slf4j.bridge.SLF4JBridgeHandler");
+                Method removeHandlersForRootLogger = bridgeHandler.getMethod("removeHandlersForRootLogger");
+                removeHandlersForRootLogger.invoke(null);
+                Method install = bridgeHandler.getMethod("install");
+                install.invoke(null);
+            } catch (Exception ex) {
+            }
         }
         // ROOT level of JUL logging should be set to ALL
         java.util.logging.Logger.getLogger("").setLevel(java.util.logging.Level.ALL);
@@ -19,11 +31,14 @@ public class SpringTest {
         java.util.logging.Logger julLogger = java.util.logging.Logger.getLogger(SpringTest.class.getName());
         julLogger.info("JUL logging...");
         julLogger.finest("JUL logging...");
-        org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SpringTest.class);
-        logger.info("SLF4J logging...");
-        logger.trace("SLF4J logging...");
-        org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(SpringTest.class);
-        log.info("JCL logging...");
-        log.trace("JCL logging...");
+        org.apache.commons.logging.Log jclLog = org.apache.commons.logging.LogFactory.getLog(SpringTest.class);
+        jclLog.info("JCL logging...");
+        jclLog.trace("JCL logging...");
+        org.apache.log4j.Logger log4jlogger = org.apache.log4j.Logger.getLogger(SpringTest.class);
+        log4jlogger.info("Log4J logging...");
+        log4jlogger.trace("Log4J logging...");
+        org.slf4j.Logger slf4jLogger = org.slf4j.LoggerFactory.getLogger(SpringTest.class);
+        slf4jLogger.info("SLF4J logging...");
+        slf4jLogger.trace("SLF4J logging...");
     }
 }
