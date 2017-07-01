556e65a7bbcfa81d1810a5dddaca7894da94836d
Alexander Golovko <alik@csltd.com.ua>
2017-07-02 00:48:15 +0300

    Log4j2 adaptation

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @chcp 1251 > nul
-call mvn clean compile exec:java -P SLF4J-LOG4J -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
+call mvn clean compile exec:java -P SLF4J-LOG4J2 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
 call mvn clean compile exec:java -P SLF4J-JDK14 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties
 call mvn clean compile exec:java -P SLF4J-Simple -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
 call mvn clean compile exec:java -P SLF4J-LogBack -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
