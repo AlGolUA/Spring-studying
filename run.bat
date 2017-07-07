48a7525f1f2602c127e77d8bf9bb8cfec24aa510
Alexander Golovko <alik@csltd.com.ua>
2017-07-07 16:56:40 +0300

    Ошибка с закрытием потока исправляется за счёт добавления -Dexec.cleanupDaemonThreads=false при запуске или определения для exec maven plugin конфигурации <configuration>        <mainClass>com.test.Startup</mainClass>        <cleanupDaemonThreads>false</cleanupDaemonThreads> </configuration>

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @chcp 1251 > nul
-call mvn clean compile exec:java -P SLF4J-LOG4J2 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
-call mvn clean compile exec:java -P SLF4J-JDK14 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties
-call mvn clean compile exec:java -P SLF4J-Simple -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
-call mvn clean compile exec:java -P SLF4J-LogBack -Dexec.mainClass="algol.csltd.com.ua.SpringTest" 
-call mvn clean compile exec:java -P WO_SLF4J -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties
+call mvn clean compile exec:java -P SLF4J-LOG4J2 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Dexec.cleanupDaemonThreads=false
+call mvn clean compile exec:java -P SLF4J-JDK14 -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties -Dexec.cleanupDaemonThreads=false
+call mvn clean compile exec:java -P SLF4J-Simple -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Dexec.cleanupDaemonThreads=false 
+call mvn clean compile exec:java -P SLF4J-LogBack -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Dexec.cleanupDaemonThreads=false 
+call mvn clean compile exec:java -P WO_SLF4J -Dexec.mainClass="algol.csltd.com.ua.SpringTest" -Djava.util.logging.config.file=target\classes\logging.properties -Dexec.cleanupDaemonThreads=false
